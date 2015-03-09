8dbde184d9bc7018e412c0e802ed12247088f6a0
Markus Teich <markus.teich@stusta.mhn.de>
2015-03-09 20:47:48 +0100

    add config documentation

--- config.def.h
+++ config.def.h
@@ -1,4 +1,10 @@
-/* exec this commands when adding a route */
+/* Every entry has to be of the form: (char *[]){STRINGLIST, (char*)NULL}
+ * The first element of STRINGLIST is the program executed, the remaining ones
+ * are the arguments passed to this program. The Argument following "-d" is
+ * replaced with the route, that is added/removed in the form 192.168.42.128/25
+ */
+
+/* exec this commands when adding a route. */
 char **const addcalls[] = {
 	(char *[]){"iptables", "-w", "-A", "filter_clearnet", "-d", "PLACEHOLDER", "-j", "ACCEPT", (char*)NULL},
 	(char *[]){"iptables", "-w", "-t", "nat", "-A", "nat_clearnet", "-d", "PLACEHOLDER", "-j", "ACCEPT", (char*)NULL}
